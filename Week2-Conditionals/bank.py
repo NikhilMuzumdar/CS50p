cfe932b2ccfac30575eb0f0fbfbcfdd5ca9286db
Nikhil Muzumdar <65347048+NikhilMuzumdar@users.noreply.github.com>
2022-08-26 12:42:11 +0530

    Delete Week2-Conditionals directory

--- Week2-Conditionals/bank.py
+++
@@ -1,9 +0,0 @@
-greeting = input("Say your greetings: ")
-greeting = greeting.strip().split()[0].lower()
-
-if greeting[:5] == "hello":
-    print("$0")
-elif greeting[0] == "h":
-    print("$20")
-else:
-    print("$100")
--- Week2-Conditionals/deep.py
+++
@@ -1,6 +0,0 @@
-string = input("What is the answer to the Great Question of Life, the Universe and Everything?: ")
-
-if string.lower().strip() in ["42", "forty-two", "forty two"]:
-    print("Yes")
-else:
-    print("No")
